18f8cf7d8aba0e372b473ee74c2857c7311fe9bd
liw57 <liw57@chinatelecom.cn>
2025-11-14 09:20:09 +0800

    liwiii's update

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- PLEASE REMOVE THE EXAMPLES YOU HAVE NO INTEREST IN BEFORE ENABLING THIS FILE
@@ -36,6 +36,8 @@ return {
             "██ ██  ██ ██    ██ ██ ██ ████ ██",
             "██  ██ ██  ██  ██  ██ ██  ██  ██",
             "██   ████   ████   ██ ██      ██",
+            "",
+            " 🤝🤝🤝 liwiii edition 🤝🤝🤝 ",
           }, "\n"),
         },
       },
